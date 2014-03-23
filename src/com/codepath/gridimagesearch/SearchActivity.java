62ca637111abad4c2c7b12c08ad3ea7725f46712
William Castellano <xstatik@gmail.com>
2014-03-22 23:36:20 -0700

    added basic image search functionality

--- src/com/codepath/gridimagesearch/SearchActivity.java
+++ src/com/codepath/gridimagesearch/SearchActivity.java
@@ -1,15 +1,45 @@
 package com.codepath.gridimagesearch;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class SearchActivity extends Activity {
+	EditText etQuery;
+	GridView gvResults;
+	Button btnSearch;
+	ArrayList<ImageResult> imageResults = new ArrayList<ImageResult>();
+	ImageResultArrayAdapter imageAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
+		setupViews();
+		imageAdapter = new ImageResultArrayAdapter(this, imageResults);
+		gvResults.setAdapter(imageAdapter);
+	}
+
+	public void setupViews() {
+		etQuery = (EditText) findViewById(R.id.etQuery);
+		gvResults = (GridView) findViewById(R.id.gvResults);
+		btnSearch = (Button) findViewById(R.id.btnSearch);
 	}
 
 	@Override
@@ -17,6 +47,31 @@ public class SearchActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.search, menu);
 		return true;
+	}
+	
+	public void onImageSearch(View v)
+	{
+		String query = etQuery.getText().toString();
+		Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get("https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + "start=" + 0 + "&v=1.0&q=" + Uri.encode(query), 
+				new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONObject response) {
+				JSONArray imageJsonResults = null;
+				try
+				{
+					imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
+					imageResults.clear();
+					imageAdapter.addAll(ImageResult.fromJSONArray(imageJsonResults));
+					Log.d("DEBUG", imageResults.toString());
+				}
+				catch (JSONException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
